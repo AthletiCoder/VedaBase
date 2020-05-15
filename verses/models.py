0793932dd09cb40c07620ed2de7c7337a007fbe7
Varun Kumar Nagandla <varun@Varuns-MacBook-Air.local>
2020-05-15 13:17:10 +0530

    models created

--- verses/models.py
+++ verses/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from . import custom_models
 
 # STATIC TABLES
 class Verse(models.Model):
@@ -6,9 +7,9 @@ class Verse(models.Model):
     canto_num = models.IntegerField(null=False)
     chapter_num = models.IntegerField(null=False)
     verse_num = models.IntegerField(null=False)
-    verse = models.TextField(max_length=200, null=False)
-    translation = models.TextField(max_length=1000, null=False)
-    purport = models.TextField(max_length=40000, null=True)
+    verse = custom_models.Text(max_length=200, null=False)
+    translation = custom_models.Text(max_length=1000, null=False)
+    purport = custom_models.Text(max_length=40000, null=True)
 
 class Tag1(models.Model):
     name = models.CharField(max_length=20)
@@ -18,15 +19,15 @@ class Tag2(models.Model):
     name = models.CharField(max_length=20)
 
 class Tag3(models.Model):
+    name = models.CharField(max_length=20, primary_key=True)
     parent_tag = models.ForeignKey(Tag2, on_delete=models.PROTECT)
-    name = models.CharField(max_length=20)
 
 class TranslationTag(models.Model):
-    verse = models.ForeignKey(Verse, on_delete=models.SET_NULL)
-    tag = models.ForeignKey(Tag3,on_delete=models.SET_NULL)
+    verse = models.ForeignKey(Verse, on_delete=models.CASCADE)
+    tag = models.ForeignKey(Tag3, on_delete=models.SET_DEFAULT, default="None")
 
 class PurportSectionTag(models.Model):
-    verse = models.ForeignKey(Verse, on_delete=models.SET_NULL)
+    verse = models.ForeignKey(Verse, on_delete=models.CASCADE)
     start_idx = models.IntegerField(null=False)
     end_idx = models.IntegerField(null=False)
-    tag = models.ForeignKey(Tag3,on_delete=models.SET_NULL)
+    tag = models.ForeignKey(Tag3,on_delete=models.SET_DEFAULT, default="None")
